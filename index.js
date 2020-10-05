f4beaaa94637f058aa8cd3b5b765554698e43a78
Chad-Daya <zeac144@live.rhul.ac.uk>
2020-10-05 14:18:14 +0100

    switch

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
+alert("Hello! I am an alert box!!");
+
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i<numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 	  
 		var buttonInnterHTML = this.innerHTML;
 
 		switch (buttonInnterHTML) {
 			case "w":
-			var audio = New Audio("sounds/tom-1.mp3");
-			audio.play();
+			var audio = New Audio('sounds/tom-1.mp3');
+			tom1.play();
 			break;
 
 			case "a":
-			var audio = New Audio("sounds/tom-2.mp3");
-			audio.play();
+			var audio = New Audio('sounds/tom-2.mp3');
+			tom2.play();
 			break;
 
 			case "s":
-			var audio = New Audio("sounds/tom-3.mp3");
-			audio.play();
+			var audio = New Audio('sounds/tom-3.mp3');
+			tom3.play();
 			break;
 
 			case "d":
-			var audio = New Audio("sounds/tom-4.mp3");
-			audio.play();
+			var audio = New Audio('sounds/tom-4.mp3');
+			tom4.play();
 			break;
 
 			case "j":
-			var audio = New Audio("sounds/snare.mp3");
-			audio.play();
+			var audio = New Audio('sounds/snare.mp3');
+			snare.play();
 			break;
 
 			case "k":
-			var audio = New Audio("sounds/crash.mp3");
-			audio.play();
+			var audio = New Audio('sounds/crash.mp3');
+			crash.play();
 			break;
 
 			case "l":
-			var audio = New Audio("sounds/kick-bass.mp3");
-			audio.play();
+			var audio = New Audio('sounds/kick-bass.mp3');
+			kick.play();
 			break;
 
 
 
 
-			default:
+			default: console.log(buttonInnterHTML); 
 
 		}
 
